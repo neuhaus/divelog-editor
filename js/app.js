47a9bb5c9e8251f1d1267b49e0f7620eaddd903c
Sven Neuhaus <neuhaus@users.noreply.github.com>
2026-07-23 23:26:31 +0200

    legalese

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-/**
- * Main Web Application Logic for divelog-editor
- * Written using British English conventions (initialise, metres, litres)
+/*
+  Main Web Application Logic for divelog-editor
+  Copyright (C) 2026 Sven Neuhaus
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 (function(window) {
   const { UNITS, CONVERSIONS, STANDARD_GASES } = window.UDDF_SCHEMA;
